3762f1f54babfbc6dab1da1facbcdd118f3a98d5
Dan Johnson <djohnson@imagescape.com>
2015-02-18 16:17:58 -0600

    Add django migrations and bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "Authlog",
-        version = "0.7",
+        version = "0.8",
         author = "Joe Jasinski",
         author_email = "jjasinski@imagescape.com",
         url = "http://www.imagescape.com",
